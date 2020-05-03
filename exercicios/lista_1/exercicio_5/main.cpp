3b46e367458c8e2b8fdb00d09e20bb0d3146bda3
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-03 10:37:27 -0300

    Adição da implementação base da árvore multivias

--- exercicios/lista_1/exercicio_5/main.cpp
+++ exercicios/lista_1/exercicio_5/main.cpp
@@ -1,8 +1,36 @@
+#include <iostream>
 #include "mtree.hpp"
 
 int main()
 {
-    
+    MTree<int> mt;
+
+    mt.push_back(2);
+    mt.push_back(3);
+    mt.push_back(6);
+
+    mt.insert(0, 23);
+    mt.insert(1, 44);
+
+    // MTree<int>* mt2 = mt.getChildren(0);
+    // mt2->push_back(23);
+    // MTree<int>* mt3 = mt2->getChildren(0);
+    // mt3->push_back(55);
+
+    // MTree<int>* mt4 = mt.getChildren(2);
+    // mt4->push_back(7);
+    // mt4->getChildren(0)->push_back(9);
+    // mt4->getChildren(0)->push_back(8);
+
+    // std::cout << mt2->countChildren() << std::endl;
+    // std::cout << mt2->getChildren(0)->data() << std::endl;
+    // std::cout << mt2->getChildren(1)->data() << std::endl; // Para testar o exception
+    // std::cout << mt2->getChildren(2)->data() << std::endl; // Para testar o exception
+
+    // std::cout << mt3->countDescendant() << std::endl;
+    // std::cout << mt3->countChildren() << std::endl;
+    // std::cout << mt.countChildren() << std::endl;    
+    // std::cout << mt.countDescendant() << std::endl;
 
     return 0;
 }
--- exercicios/lista_1/exercicio_5/mtree.hpp
+++ exercicios/lista_1/exercicio_5/mtree.hpp
@@ -4,6 +4,7 @@
 // Vector selecionado por conta do O(1) na seleção
 #include <vector>
 #include <cstddef>
+#include <stdexcept>
 
 template<class T>
 class MTree
@@ -12,17 +13,21 @@ public:
     MTree(MTree* parent = nullptr);
     MTree(T data, MTree* parent = nullptr);
 
-    const MTree* getParent();
     void push_back(T data);
+    MTree* getParent() const;
     void insert(std::size_t tid, T data);
 
-    const std::vector<MTree*> descendants();
+    std::size_t countChildren() const;
+    std::size_t countDescendant() const;
+    MTree* getChildren(std::size_t tid) const;
+
+    T data() const;
 private:
     int p_tid;
     T p_data;
 
     MTree *p_parent;
-    std::vector<MTree*> p_descendants;
+    std::vector<MTree*> p_children;
 };
 
 template<class T>
@@ -34,15 +39,67 @@ MTree<T>::MTree(T data, MTree* parent): p_data(data), p_parent(parent) {}
 template<class T>
 void MTree<T>::push_back(T data)
 {
-    p_descendants.push_back(new MTree<T>(data, this));
+    p_children.push_back(new MTree<T>(data, this));
 }
 
 template<class T>
 void MTree<T>::insert(std::size_t tid, T data)
 {
-    MTree* tmp = new MTree(data);
+    if (tid >= countChildren())
+        throw std::runtime_error("Índice inválido!");
 
-    // adicionar possibilidade de não sobrescrever a posição.
+    p_children[tid]->push_back(data);
+}
+
+template<class T>
+std::size_t MTree<T>::countChildren() const
+{
+    return p_children.size();
+}
+
+template<class T> 
+MTree<T>* MTree<T>::getParent() const
+{
+    return p_parent;
+}
+
+template<class T>
+MTree<T>* MTree<T>::getChildren(std::size_t tid) const
+{
+    if (tid >= countChildren())
+        throw std::runtime_error("Índice inválido");
+    return p_children[tid];
+}
+
+template<class T>
+std::size_t countDescendantRecursive(const MTree<T>* root)
+{
+    std::size_t res = 0;
+    std::size_t childrenSize = root->countChildren();
+    if (childrenSize == 0)
+        return 1;
+
+    for(std::size_t i = 0; i < childrenSize; ++i)
+    {
+        res += countDescendantRecursive(root->getChildren(i));
+    }
+
+    // A soma é feita para considerar a raiz atual no cálculo dos descendentes
+    return res + 1;
+}
+
+template<class T>
+std::size_t MTree<T>::countDescendant() const
+{
+    // A subtração é feita já que, na recursão a raiz principal 
+    // é considerada, então, esta subtração remove esta consideração
+    return countDescendantRecursive(this) - 1;
+}
+
+template<class T>
+T MTree<T>::data() const
+{
+    return p_data;
 }
 
 #endif
