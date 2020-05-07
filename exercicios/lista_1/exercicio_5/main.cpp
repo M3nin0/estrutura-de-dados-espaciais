6d6d474c368f65217322ca517b2480a3474524ad
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-07 00:36:43 -0300

    Correção do main da lista 5

--- exercicios/lista_1/exercicio_5/main.cpp
+++ exercicios/lista_1/exercicio_5/main.cpp
@@ -12,25 +12,25 @@ int main()
     mt.insert(0, 23);
     mt.insert(1, 44);
 
-    // MTree<int>* mt2 = mt.getChildren(0);
-    // mt2->push_back(23);
-    // MTree<int>* mt3 = mt2->getChildren(0);
-    // mt3->push_back(55);
+    MTree<int>* mt2 = mt.getChildren(0);
+    mt2->push_back(23);
+    MTree<int>* mt3 = mt2->getChildren(0);
+    mt3->push_back(55);
 
-    // MTree<int>* mt4 = mt.getChildren(2);
-    // mt4->push_back(7);
-    // mt4->getChildren(0)->push_back(9);
-    // mt4->getChildren(0)->push_back(8);
+    MTree<int>* mt4 = mt.getChildren(2);
+    mt4->push_back(7);
+    mt4->getChildren(0)->push_back(9);
+    mt4->getChildren(0)->push_back(8);
 
-    // std::cout << mt2->countChildren() << std::endl;
-    // std::cout << mt2->getChildren(0)->data() << std::endl;
+    std::cout << mt2->countChildren() << std::endl;
+    std::cout << mt2->getChildren(0)->data() << std::endl;
     // std::cout << mt2->getChildren(1)->data() << std::endl; // Para testar o exception
     // std::cout << mt2->getChildren(2)->data() << std::endl; // Para testar o exception
 
-    // std::cout << mt3->countDescendant() << std::endl;
-    // std::cout << mt3->countChildren() << std::endl;
-    // std::cout << mt.countChildren() << std::endl;    
-    // std::cout << mt.countDescendant() << std::endl;
+    std::cout << mt3->countDescendant() << std::endl;
+    std::cout << mt3->countChildren() << std::endl;
+    std::cout << mt.countChildren() << std::endl;    
+    std::cout << mt.countDescendant() << std::endl;
 
     return 0;
 }
