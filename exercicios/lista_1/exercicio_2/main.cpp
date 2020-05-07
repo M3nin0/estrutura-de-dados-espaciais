bdfcabeccbf1cad702b74cf5bd1b75903441a368
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-07 01:07:40 -0300

    Melhoria nos exemplos do main.cpp

--- exercicios/lista_1/exercicio_2/main.cpp
+++ exercicios/lista_1/exercicio_2/main.cpp
@@ -45,7 +45,34 @@ int main()
 
     list<complex> l3;
     l3.insert(l3.begin(), complex());
+    // Este comando foi adicionado para mostrar que, caso 
+    // a estrutura de dados não tenha os operadores de comparação implementados
+    // problemas serão apresentados
     // l3.find(complex()); // erro
+
+    std::cout << "Merge" << std::endl;
+    l.clear();
+    l2.clear();
+
+    l.push_front(1); l.push_front(2);
+    l2.push_front(99); l2.push_front(22);l2.push_front(106);
+
+    std::cout << "- Antes do merge" << std::endl;
+    print<int>(l);
+    print<int>(l2);
+    std::cout << "- Depois do merge" << std::endl;
+    l.merge(l2);
+    print<int>(l);
+    print<int>(l2);
+
+    std::cout << "Splice" << std::endl;
+    l2.push_back(567); l2.push_back(789);
+    l.splice(l2);
+    print<int>(l);
+
+    std::cout << "Mostrando a lista com merge e splice invertida" << std::endl;
+    l.reverse();
+    print<int>(l);
 
     return 0;
 }
