8d8048d22f0bd0a37df8b6f13ac6a67b23a0e774
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-15 00:29:46 -0300

    Adição do sort com MaxHeap

--- anotacoes/2_heaps/maxheap/heap.hpp
+++ anotacoes/2_heaps/maxheap/heap.hpp
@@ -9,8 +9,10 @@ template <class T>
 class MaxHeap
 {
 public:
+    MaxHeap();
     MaxHeap(const std::vector<T>& vec);
 
+    void Sort(std::vector<T>& vec);
     std::vector<T> ExtractHeap(std::size_t n);
 
     std::size_t Begin() const;
@@ -19,8 +21,14 @@ private:
     std::vector<T> p_vec;
 
     void FixUpHeap(T elm, std::size_t i);
+    void swap(std::vector<T>& vec1, std::size_t pos1, std::size_t pos2);
     void FixDownHeap(std::size_t limitToRemove, std::size_t actualPosition);
 };
+
+template <class T>
+MaxHeap<T>::MaxHeap()
+{
+}
 
 /**
  * Método para a criação de um heap através de um array
@@ -75,7 +83,7 @@ std::vector<T> MaxHeap<T>::ExtractHeap(std::size_t limitToRemove)
 {
     std::vector<T> sortedElements;
 
-    for(std::size_t i = 0; i < limitToRemove; ++i)
+    for(std::size_t i = 1; i <= limitToRemove; ++i)
     {
         sortedElements.push_back(p_vec[0]); // max-heap(raiz = valor máximo)
         p_vec[0] = p_vec[limitToRemove - i]; // a raiz passa a ser o último elemento da sequência
@@ -85,9 +93,15 @@ std::vector<T> MaxHeap<T>::ExtractHeap(std::size_t limitToRemove)
                                        // onde cada elemento '-1' representa uma posição removida
     }
 
-    return sortedElements;
+    return sortedElements; // em ordem inversa
 }
 
+/**
+ * Algoritmo que trata de reorganizar o Heap quando o maior elemento é removido.
+ * Neste, a substituição entre a raiz e o topo é feita em seguida, o elemento que 
+ * está no topo é empurrado para baixo até que esteja na sua posição correta 
+ * (Quando não é menor que nínguem)
+ */
 template <class T>
 void MaxHeap<T>::FixDownHeap(std::size_t limitToRemove, std::size_t actualPosition)
 {
@@ -99,12 +113,15 @@ void MaxHeap<T>::FixDownHeap(std::size_t limitToRemove, std::size_t actualPositi
 
     while (j <= limitToRemove)
     {
+        // A pergunta respondida na linha abaixo é: Qual dos dois possuí chave maior ?
         if ((j < limitToRemove) && (p_vec[j] < p_vec[j + 1]))
             ++j; // se eu estou dentro do range definido para remoção
                 // e, descubro que o filho direito é maior que o esquerdo
                 // somo +1 ao j para, nas linhas abaixo trabalhar com o elemento à direita,
                 // caso contrário, o elemento trabalhado permanece sendo o esquerdo.
 
+        // A pergunta respondida na linha abaixo é: 
+        // O elemento que está no topo é maior do que seu maior filho ?
         if (v > p_vec[j]) // se a posição onde estou é maior que meu maior filho
                           // cheguei no ponto em que queria, assim, posso parar a 'varredura'
             break;
@@ -112,7 +129,40 @@ void MaxHeap<T>::FixDownHeap(std::size_t limitToRemove, std::size_t actualPositi
                                  // seu filho.
         j *= 2; // passa para a próxima raiz
     }
-    p_vec[j / 2] = v;
+    p_vec[j / 2] = v; // o pai da posição onde eu estou passa a ser
+                      // o valor de 'v'.
+}
+
+template <class T>
+void MaxHeap<T>::Sort(std::vector<T>& vec)
+{
+    // inserindo vetor para a ordenação
+    // vec.size() = n operações
+    p_vec = vec;
+    for(std::size_t i = 0; i < vec.size(); ++i)
+    {
+        FixUpHeap(p_vec.at(i), i);
+    }
+
+    // Fazendo o caminho inverso do feito no método "FixDownHeap"
+    // Posso fazer isso já que meu método consegue trabalhar com a posição
+    // de onde eu considero uma raiz. O que fiz aqui foi 'inverter' a árvore,
+    // ou melhor dizendo, inverti a ordem com que leio a árvore.
+    for(std::size_t i = vec.size() - 1; i > 0; --i)
+    {
+        swap(p_vec, 0, i);
+        FixDownHeap(i - 1, 0);
+    }
+
+    vec = p_vec;
+}
+
+template <class T>
+void MaxHeap<T>::swap(std::vector<T>& vec1, std::size_t pos1, std::size_t pos2)
+{
+    T tmp = vec1.at(pos1);
+    vec1[pos1] = vec1[pos2];
+    vec1[pos2] = tmp;
 }
 
 #endif
--- anotacoes/2_heaps/maxheap/main.cpp
+++ anotacoes/2_heaps/maxheap/main.cpp
@@ -1,5 +1,6 @@
 #include "heap.hpp"
 #include <vector>
+#include <algorithm>
 
 template <class T>
 inline void PrintVector(std::vector<T>& vec)
@@ -24,6 +25,15 @@ int main()
     PrintVector(vec);
     std::cout << "Depois de sortear" << std::endl;
     PrintVector(sortedVec);
+    std::cout << "Foi sorteado corretamente ?" << std::is_sorted(sortedVec.begin(), sortedVec.end()) << std::endl;
+
+    // Testando sort crescente
+    std::cout << "Sort com ordem inversa" << std::endl;
+    std::vector<int> vec2({98, 17, 86, 11, 16, 78, 85, 5, 8, 13, 9, 35, 46, 18, 70});
+
+    MaxHeap<int> maxHeapSort;
+    maxHeapSort.Sort(vec2);
+    std::cout << "Foi sorteado corretamente ? " << std::is_sorted(vec2.begin(), vec2.end()) << std::endl;
 
     return 0;
 }
